2bc7604dcd36f0d6a9f88c3c8c67e4e6a79e5d48
matsuyamarikiya <maturiki17@ymail.ne.jp>
2026-06-12 09:05:29 +0900

    feat: 投稿一覧サイドバーにカテゴリーリンクと件数表示を実装

--- category.php
+++ category.php
@@ -22,73 +22,25 @@
   <section class="news js-header-trigger">
     <div class="news__inner">
 
-      <!-- side var -->
-      <aside class="l-sidebar">
-        <section class="l-sidebar__section">
-          <h2 class="l-sidebar__title">Category</h2>
-
-          <ul class="l-sidebar__list">
-            <li class="l-sidebar__item">
-              <a href="<?php echo esc_url(get_permalink(get_option('page_for_posts'))); ?>" class="l-sidebar__link">
-                <span class="l-sidebar__label">すべて</span>
-                <span class="l-sidebar__count">(52)</span>
-              </a>
-            </li>
-
-            <li class="l-sidebar__item">
-              <a href="#" class="l-sidebar__link">
-                <span class="l-sidebar__label">お知らせ</span>
-                <span class="l-sidebar__count">(23)</span>
-              </a>
-            </li>
-          </ul>
-        </section>
-
-        <section class="l-sidebar__section">
-          <h2 class="l-sidebar__title">Archive</h2>
-
-          <ul class="l-sidebar__list">
-            <li class="l-sidebar__item">
-              <a href="#" class="l-sidebar__link">
-                <span class="l-sidebar__label">2026年</span>
-                <span class="l-sidebar__count">(10)</span>
-              </a>
-            </li>
-            <li class="l-sidebar__item">
-              <a href="#" class="l-sidebar__link">
-                <span class="l-sidebar__label">2025年</span>
-                <span class="l-sidebar__count">(10)</span>
-              </a>
-            </li>
-          </ul>
-        </section>
-      </aside>
+      <!-- side bar -->
+      <?php get_template_part('template-parts/news-sidebar'); ?>
 
       <!-- list -->
       <div class="news__list-wrap">
         <ul class="c-news__list news__list">
           <li class="news__item">
-            <article class="c-news__item">
-              <a href="/aoyama-accounting/frontend/news/detail/" class="c-news__link" aria-label="詳細ページへ">
+            <?php
+            if (have_posts()) :
+              while (have_posts()) :
+                the_post();
+            ?>
 
-                <!-- img -->
-                <div class="c-news__img">
-                  <img src="/aoyama-accounting/frontend/news/img/news.jpg" width="315" height="210" alt="">
-                </div>
+                <?php get_template_part('template-parts/post-loop-news'); ?>
 
-                <!-- content -->
-                <div class="c-news__content">
-                  <div class="c-news__info">
-                    <span class="c-news__category">お知らせ</span>
-                    <time datetime="2026-01-08" class="c-news__date">2026.01.08</time>
-                  </div>
-
-                  <h2 class="c-news__title">
-                    ホームページをリニューアルしました
-                  </h2>
-                </div>
-              </a>
-            </article>
+            <?php
+              endwhile;
+            endif;
+            ?>
           </li>
         </ul>
 
